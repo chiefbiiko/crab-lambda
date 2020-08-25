414ea0a80efd6926e1391cdb9abd74d86762e71b
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-08-25 12:28:56 +0200

    build: reallo pkg cross

--- examples/easy/src/lib.rs
+++ examples/easy/src/lib.rs
@@ -1,10 +1,11 @@
 use anyhow::Result as Rezult;
-use serde_json::json;
+// use serde_json::json;
 
 #[no_mangle]
 pub fn lambda(event: String, context: String) -> Rezult<String> {
-    println!("{}", event);
-    println!("{}", context);
+    println!("{}", &event);
+    println!("{}", &context);
 
-    Ok(json!({ "fraud": "money" }).to_string())
+    // Ok(json!({ "fraud": "money" }).to_string())
+    Ok("{\"fraud\":\"money\"}".to_string())
 }
