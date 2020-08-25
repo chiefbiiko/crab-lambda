f421a6bedb0f9f8c066abeb920d3965e0bbb7a11
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-08-25 17:17:02 +0200

    build: try cheating

--- examples/easy/src/lib.rs
+++ examples/easy/src/lib.rs
@@ -1,5 +1,7 @@
 use anyhow::Result as Rezult;
 // use serde_json::json;
+use core::panic;
+// https://blog-os.netlify.app/freestanding-rust-binary/
 
 #[no_mangle]
 pub fn lambda(event: String, context: String) -> Rezult<String> {
@@ -8,4 +10,9 @@ pub fn lambda(event: String, context: String) -> Rezult<String> {
 
     // Ok(json!({ "fraud": "money" }).to_string())
     Ok("{\"fraud\":\"money\"}".to_string())
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    panic!(_info);
 }
