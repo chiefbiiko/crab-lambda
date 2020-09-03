98f1ed59103deb9d8b05b4963852c74c00fe82fb
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2020-09-03 15:02:52 +0200

    impl: try supllying cryptic lang feats

--- examples/easy/src/lib.rs
+++ examples/easy/src/lib.rs
@@ -1,6 +1,9 @@
 use anyhow::Result as Rezult;
 // use serde_json::json;
 
+use core::intrinsics;
+use core::panic::PanicInfo;
+
 #[no_mangle]
 pub fn lambda(event: String, context: String) -> Rezult<String> {
     println!("{}", &event);
@@ -9,3 +12,6 @@ pub fn lambda(event: String, context: String) -> Rezult<String> {
     // Ok(json!({ "fraud": "money" }).to_string())
     Ok("{\"fraud\":\"money\"}".to_string())
 }
+
+#[lang = "eh_personality"] extern fn "C" rust_eh_personality() {}
+#[lang = "panic_impl"] extern fn "C" rust_begin_panic(info: &PanicInfo) -> ! { unsafe { intrinsics::abort() } }
