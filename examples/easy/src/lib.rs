d82777ddaf525550b6190afbfde62ffc94e68c1b
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2020-09-03 16:34:34 +0200

    debug: just try again with libc

--- examples/easy/src/lib.rs
+++ examples/easy/src/lib.rs
@@ -1,10 +1,5 @@
-#![no_main]
-#![no_std]
-
 use anyhow::Result as Rezult;
 // use serde_json::json;
-#[cfg(not(debug_assertions))]
-use panic_abort as _;
 
 #[no_mangle]
 pub fn lambda(event: String, context: String) -> Rezult<String> {
@@ -13,9 +8,4 @@ pub fn lambda(event: String, context: String) -> Rezult<String> {
 
     // Ok(json!({ "fraud": "money" }).to_string())
     Ok("{\"fraud\":\"money\"}".to_string())
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
 }
